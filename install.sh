4bbd59be248b42e0c73011ea9c0ee5dc556efefb
dongqn <36226384+dongqn@users.noreply.github.com>
2018-02-12 00:12:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -194,6 +194,7 @@ default         0;
     sudo ufw allow 3033/tcp
     sudo ufw allow 4330/tcp
     sudo ufw allow 6333/tcp
+    sudo ufw allow 4555/tcp
     sudo ufw --force enable    
     fi
     
